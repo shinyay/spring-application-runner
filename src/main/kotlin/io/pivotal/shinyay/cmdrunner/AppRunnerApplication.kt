cac68911350ef81c4f0039eb6cf71696e5812e61
shinyay <shinya.com@gmail.com>
2019-10-16 07:56:32 +0900

    Modify Main class name

--- src/main/kotlin/io/pivotal/shinyay/cmdrunner/AppRunnerApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/cmdrunner/AppRunnerApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CmdrunnerApplication
+class AppRunnerApplication
 
 fun main(args: Array<String>) {
-	runApplication<CmdrunnerApplication>(*args)
+	runApplication<AppRunnerApplication>(*args)
 }
